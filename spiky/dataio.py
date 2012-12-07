work in progress: adding the baselines. the computation of the baselines appears to be wrong currently

spiky/dataio.py
@@ -107,9 +107,9 @@ def brian(T1, T2, width=.02, bin=.001, T=None):
     return H / W
 
 
-def get_correlogram(x, y, width=.021, bin=.001):
+def get_correlogram(x, y, width=.021, bin=.001, duration=None):
     # TODO: this is highly unoptimized, optimize that
-    corr = brian(x, y, width=width, bin=bin)
+    corr = brian(x, y, width=width, bin=bin, T=duration)
     # print corr
     if corr is None:
         return None
@@ -132,33 +132,47 @@ class CorrelogramManager(object):
         self.histlen = 2 * int(np.ceil(width / bin))
         # cache correlograms
         self.correlograms = {}
+        self.spikecounts = {}
         
     def reset(self):
         self.correlograms.clear()
+        self.spikecounts.clear()
     
     def invalidate(self, clusters):
         """Remove from the cache all correlograms related to the given
         clusters."""
         correlograms_new = {}
+        spikecounts_new = {}
         # copy in the new dictionary all correlograms which do not refer
         # to clusters in the given list of invalidated clusters
         for (i, j), corr in self.correlograms.iteritems():
             if i not in clusters and j not in clusters:
+                # update correlograms
                 correlograms_new[(i, j)] = self.correlograms[(i, j)]
+                # update spike counts too
+                spikecounts_new[i] = self.spikecounts[i]
+                spikecounts_new[j] = self.spikecounts[j]
         self.correlograms = correlograms_new
+        self.spikecounts = spikecounts_new
     
     def compute(self, cluster0, cluster1=None):
         if cluster1 is None:
             cluster1 = cluster0
+
         x = self.sdh.get_spiketimes(cluster0)
         y = self.sdh.get_spiketimes(cluster1)
         
-        # convert into floats
+        # convert spike train units from samples counts to seconds
         x = x * 1. / self.dh.freq
         y = y * 1. / self.dh.freq
         
-        corr = get_correlogram(x, y, width=self.width, bin=self.bin)
+        corr = get_correlogram(x, y, width=self.width, bin=self.bin,
+            duration=self.dh.duration)
         self.correlograms[(cluster0, cluster1)] = corr
+
+        self.spikecounts[cluster0] = len(x)
+        self.spikecounts[cluster1] = len(y)
+
         return corr
         
     def get_correlogram(self, cluster0, cluster1=None):
@@ -182,7 +196,10 @@ class CorrelogramManager(object):
         # print len(correlograms)
         # print [len(c) for c in correlograms]
         return np.vstack(correlograms)
-        
+    
+    def get_spikecounts(self, clusters):
+        return np.array([self.spikecounts[i] for i in clusters])
+    
     
 # Data holder
 # -----------
@@ -235,6 +252,9 @@ class SelectDataHolder(object):
     def get_correlograms(self, clusters):
         return self.corr_manager.get_correlograms(clusters)
             
+    def get_spikecounts(self, clusters):
+        return self.corr_manager.get_spikecounts(clusters)
+            
     def _selector_ufunc(self, clusters=None):
         """Create a custom ufunc for cluster selection."""
         if clusters is None or len(clusters) == 0:
@@ -253,6 +273,15 @@ class SelectDataHolder(object):
         self.nspikes = select_mask.sum()
         # TODO: move that outside dataio
         self.correlograms = self.get_correlograms(clusters)
+        
+        counts = self.get_spikecounts(clusters)
+        counts = np.array([counts[i] for i in xrange(len(clusters)) 
+                        for j in xrange(i, len(clusters))])
+        
+        # print counts
+        # print self.correlograms
+        
+        self.baselines = counts / float(self.dataholder.duration)
         self.nclusters = len(clusters)
         self.cluster_colors = self.dataholder.clusters_info.colors[clusters]
         # unique clusters
@@ -333,6 +362,9 @@ class KlustersDataProvider(DataProvider):
         
         # construct spike times from random interspike interval
         self.holder.spiketimes = spiketimes
+        
+        # TODO
+        self.holder.duration = spiketimes[-1] / float(self.holder.freq)
         
         self.holder.waveforms = waveforms
         self.holder.waveforms_info = Info(nsamples=nsamples)
@@ -449,6 +481,8 @@ class MockDataProvider(DataProvider):
         self.holder.nspikes = nspikes
         self.holder.nclusters = nclusters
         self.holder.nchannels = nchannels
+        
+        self.holder.duration = 100.
         
         # construct spike times from random interspike interval
         self.holder.spiketimes = np.cumsum(np.random.randint(size=nspikes,

spiky/views/correlogramsview.py
@@ -55,7 +55,7 @@ def get_histogram_points(hist):
 
 
 class CorrelogramsDataManager(Manager):
-    def set_data(self, histograms=None, cluster_colors=None):
+    def set_data(self, histograms=None, cluster_colors=None, baselines=None):
         
         # self.clusters = clusters
         # self.nclusters = nclusters
@@ -84,11 +84,11 @@ class CorrelogramsDataManager(Manager):
         self.position = np.empty((n, 2), dtype=np.float32)
         self.position[:,0] = X.ravel()
         self.position[:,1] = Y.ravel()
+
+        # print baselines
         
-        
-        # TODO
-        self.baselines = np.zeros(self.nhistograms)
-        
+        # baselines of the correlograms
+        self.baselines = baselines
         
         # cluster i and j for each histogram in the view
         clusters = [(i,j) for i in xrange(self.nclusters) for j in xrange(self.nclusters) if j >= i]
@@ -102,7 +102,7 @@ class CorrelogramsDataManager(Manager):
         else:
             identity = []
         
-        color_array_index[identity] = cluster_colors + 1
+        color_array_index[identity] = np.array(cluster_colors + 1, dtype=np.int32)
         self.color = np.vstack((np.ones((1, 3)), COLORMAP))
         self.color_array_index = color_array_index
         
@@ -131,6 +131,7 @@ class CorrelogramsVisual(PlotVisual):
         self.add_uniform("nclusters", vartype="int", ndim=1, data=nclusters)
         
         self.add_vertex_main(VERTEX_SHADER)
+
         
 class CorrelogramsBaselineVisual(PlotVisual):
     def initialize(self, nclusters=None, baselines=None, clusters=None):
@@ -175,11 +176,11 @@ class CorrelogramsPaintManager(PaintManager):
             clusters=self.data_manager.clusters,
             name='correlograms')
             
-        self.add_visual(CorrelogramsBaselineVisual,
-            nclusters=self.data_manager.nclusters,
-            clusters=self.data_manager.clusters0,
-            baselines=self.data_manager.baselines,
-            name='correlograms_baseline')
+        # self.add_visual(CorrelogramsBaselineVisual,
+            # nclusters=self.data_manager.nclusters,
+            # clusters=self.data_manager.clusters0,
+            # baselines=self.data_manager.baselines,
+            # name='correlograms_baseline')
         
         
         
@@ -196,12 +197,12 @@ class CorrelogramsPaintManager(PaintManager):
             clusters=self.data_manager.clusters,
             visual='correlograms')
             
-        self.reinitialize_visual(
-            size=2 * len(self.data_manager.clusters0),
-            nclusters=self.data_manager.nclusters,
-            baselines=self.data_manager.baselines,
-            clusters=self.data_manager.clusters0,
-            visual='correlograms_baseline')
+        # self.reinitialize_visual(
+            # size=2 * len(self.data_manager.clusters0),
+            # nclusters=self.data_manager.nclusters,
+            # baselines=self.data_manager.baselines,
+            # clusters=self.data_manager.clusters0,
+            # visual='correlograms_baseline')
             
             
         
@@ -214,7 +215,7 @@ class CorrelogramsBindings(SpikyDefaultBindingSet):
 class CorrelogramsView(GalryWidget):
     def initialize(self):
         self.constrain_ratio = True
-        self.constrain_navigation = True
+        # self.constrain_navigation = True
         self.set_bindings(CorrelogramsBindings)
         self.set_companion_classes(paint_manager=CorrelogramsPaintManager,
             data_manager=CorrelogramsDataManager,)