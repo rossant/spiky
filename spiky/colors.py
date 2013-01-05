refined choice of colors fixed normalization issue in feature view: now all features are completely normalized at initialization, using the min and max of all values across all spikes, all channels, and all projections fixed tab order in channel boxes in feature view

spiky/colors.py
@@ -23,7 +23,8 @@ def hsv_to_rgb(HSV):
 def generate_hsv(n0=20):
     H = np.linspace(0., 1., n0)
     i = np.arange(n0)
-    H = H[~((i==5) | (i==7) | (i==10) | (i==12) | (i==19))]
+    H = H[~((i==5) | (i==7) | (i==10) | (i==12) | (i==15) |(i==17) | (i==18) | (i==19))]
+    # H = H[((i==15) |(i==17) | (i==18) | (i==19))]
 
     H = np.repeat(H, 2)
     
@@ -46,7 +47,7 @@ def generate_hsv(n0=20):
 COLORS = hsv_to_rgb(generate_hsv())
 COLORS = np.clip(COLORS, 0, 1)
 COLORS_COUNT = len(COLORS)
-step = 11
+step = 17  # needs to be prime with COLORS_COUNT
 perm = np.mod(np.arange(0, step * COLORS_COUNT, step), COLORS_COUNT)
 COLORS = COLORS[perm,...]
 COLORMAP = np.array(COLORS)
@@ -75,26 +76,14 @@ if __name__ == "__main__":
     
     from galry import *
     
-    # H = np.linspace(0., 1., 20)
-    # H = np.repeat(H, 2)
-    # n = len(H)
-    # S = np.ones(n)
-    # V = np.ones(n)
-    # V[1::2] = .75
-    
-    # hsv = np.zeros((n, 3))
-    # hsv[:,0] = H
-    # hsv[:,1] = S
-    # hsv[:,2] = V
-    
     hsv = generate_hsv()
     
     figure(constrain_navigation=False)
     
     hsv_rect(hsv, (-1,0,1,1))
     
-    hsv[:,1] = 0.5 # white -> color
-    # hsv[:,2] = .5 # black -> white
+    hsv[:,1] -= 0.5 # white -> color
+    hsv[:,2] += 0.5 # black -> white
     hsv_rect(hsv, (-1,-1,1,0))
     
     ylim(-1,1)

spiky/dataio.py
@@ -381,9 +381,21 @@ class KlustersDataProvider(DataProvider):
         clusters = clusters[1:]
         
         features = load_text(filename + ".fet.1", np.int32, skiprows=1)
+        features = np.array(features, dtype=np.float32)
         features = features.reshape((-1, 97))
-        spiketimes = features[:,-1]
-        # features = features[:,:-1]
+        # get the spiketimes
+        spiketimes = features[:,-1].copy()
+        # remove the last column in features, containing the spiketimes
+        features = features[:,:-1]
+        # normalize the data here
+        # dn = DataNormalizer(np.array(features, dtype=np.float32))
+        # features = dn.normalize(symmetric=True)
+        m = features.min()
+        M = features.max()
+        # force symmetry
+        vx = max(np.abs(m), np.abs(M))
+        m, M = -vx, vx
+        features = -1+2*(features-m)/(M-m)
         
         try:
             masks = load_text(filename + ".mask.1", np.float32, skiprows=1)