654c4bf4e5eb5630aafde134f82524459f7b726f
Cyrille Rossant <cyrille.rossant@github.com>
2013-01-07 16:49:07 +0000

    new "scripts" folder, with a run script to launch spiky, and optionally a file as a command-line argument

---
+++ scripts/run.py
@@ -0,0 +1,25 @@
+import sys
+import re
+import numpy as np
+from spiky import *
+
+if __name__ == '__main__':
+    filename = ""
+    if len(sys.argv) >= 2:
+        filename = sys.argv[1]
+        
+    r = re.search(r"([^\n]+)\.[^\.]+\.[0-9]+$", filename)
+    if r:
+        filename = r.group(1)
+        
+    if filename:
+        class MySpiky(SpikyMainWindow):
+            def initialize_data(self):
+                provider = KlustersDataProvider()
+                self.dh = provider.load(filename)
+                self.sdh = SelectDataHolder(self.dh)
+    else:
+        MySpiky = SpikyMainWindow
+        
+    window = show_window(MySpiky)
+    
