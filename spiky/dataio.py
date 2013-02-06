display the number of selected spikes when doing a selection in the feature view fixed bug in waveform view related to box sizes computation of the correlation matrix now happens on a background thread cluster info is now associated to the Shift key added CorrelationMatrix to the Actions menu

spiky/dataio.py
@@ -289,6 +289,19 @@ class ClusterCache(object):
         needs to be updated."""
         signals.emit(self, 'CorrelogramsUpdated')
         
+       
+@qtjobqueue
+class CorrelationMatrixQueue(object):
+    def __init__(self, dh):
+        self.dh = dh
+        
+    def process(self):
+        self.dh.correlation_matrix = correlation_matrix(
+            self.dh.features, self.dh.clusters)
+        # import time
+        # time.sleep(5)
+        signals.emit(self, 'CorrelationMatrixUpdated')
+        
         
     
     
@@ -323,6 +336,9 @@ class DataHolder(object):
         filtered_trace: like raw trace, but with the filtered trace
         filter_info: a FilterInfo dic
     """
+    def __init__(self):
+        self.correlation_matrix = np.zeros((2, 2))
+    
     def new_cluster(self):
         ids = self.clusters_info['clusters_info'].keys()
         return max(ids) + 1
@@ -634,8 +650,9 @@ class KlustersDataProvider(DataProvider):
         # self.holder.correlation_matrix = rdn.rand(nclusters, nclusters)
         # self.holder.correlation_matrix = np.array([[]])
         # features = 
-        self.holder.correlation_matrix = correlation_matrix(features, clusters)
-        
+        # self.holder.correlation_matrix = correlation_matrix(features, clusters)
+        self.holder.correlation_matrix_queue = CorrelationMatrixQueue(self.holder)
+        self.holder.correlation_matrix_queue.process()
         
         return self.holder
         

spiky/views/featureview.py
@@ -702,17 +702,11 @@ class FeatureBindings(SpikyBindings):
         self.set('Wheel', 'SelectNeighborChannel',
                  key_modifier='Control',
                  param_getter=lambda p: (0, -int(np.sign(p['wheel']))))
-        # self.set('Wheel', 'SelectNeighborChannel',
-                 # key_modifier='Control',
-                 # param_getter=lambda p: (0, 1))
                  
         # select previous/next channel for coordinate 1
         self.set('Wheel', 'SelectNeighborChannel',
                  key_modifier='Shift',
                  param_getter=lambda p: (1, -int(np.sign(p['wheel']))))
-        # self.set('Wheel', 'SelectNeighborChannel',
-                 # key_modifier='Shift',
-                 # param_getter=lambda p: (1, 1))
         
     def set_neighbor_feature(self):
         # select previous/next feature for coordinate 0
@@ -732,7 +726,7 @@ class FeatureBindings(SpikyBindings):
                  param_getter=lambda p: (1, 1))
         
     def set_clusterinfo(self):
-        self.set('Move', 'ShowClosestCluster', key_modifier='Control',
+        self.set('Move', 'ShowClosestCluster', key_modifier='Shift',
             param_getter=lambda p:
             (p['mouse_position'][0], p['mouse_position'][1]))
         
@@ -875,11 +869,15 @@ class FeatureWidget(VisualizationWidget):
         ssignals.SIGNALS.ProjectionChanged.connect(self.slotProjectionChanged, QtCore.Qt.UniqueConnection)
         ssignals.SIGNALS.ClusterSelectionChanged.connect(self.slotClusterSelectionChanged, QtCore.Qt.UniqueConnection)
         ssignals.SIGNALS.HighlightSpikes.connect(self.slotHighlightSpikes, QtCore.Qt.UniqueConnection)
+        ssignals.SIGNALS.SelectSpikes.connect(self.slotSelectSpikes, QtCore.Qt.UniqueConnection)
         
     def slotHighlightSpikes(self, sender, spikes):
         self.update_nspikes_viewer(self.dh.nspikes, len(spikes))
         if sender != self.view:
             self.view.highlight_spikes(spikes)
+        
+    def slotSelectSpikes(self, sender, spikes):
+        self.update_nspikes_viewer(self.dh.nspikes, len(spikes))
         
     def slotClusterSelectionChanged(self, sender, clusters):
         self.update_view()