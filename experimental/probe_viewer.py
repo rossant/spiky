8234147da6ef6350c6f5bf1d0231b369a909cfe3
Cyrille Rossant <cyrille.rossant@github.com>
2013-02-21 14:06:12 +0000

    updated probe viewer

--- experimental/probe_viewer.py
+++ experimental/probe_viewer.py
@@ -40,8 +40,15 @@ def get_disc(size):
     
 class Probe(Manager):
     
+    def clear(self):
+        self.edges = np.zeros((0, 2), dtype=np.int32)
+        self.positions = np.zeros((1, 2))
+        self.shanks = np.zeros(1, dtype=np.int32)
+        self.edge_adding = None
+    
     def load(self, imgname=None):
         self.image = imread(imgname)
+        self.edge_adding = None
 
     def save(self, filename):
         np.savetxt(filename, self.get_positions(), fmt='%.3f')
@@ -50,14 +57,35 @@ class Probe(Manager):
         self.positions = np.vstack((self.get_positions(), position))
         self.shanks = np.hstack((self.get_shanks(), shank))
         
+    def add_edge(self, node0, node1):
+        self.edges = np.vstack((self.get_edges(), (node0, node1)))
+        self.edge_adding = None
+        
+    def adding_edge(self, node0, position):
+        self.edge_adding = (node0, position)
+        
+    def cancel_adding_edge(self):
+        self.edge_adding = None
+        
     def remove_channel(self, channelidx):
+        if channelidx is None:
+            return
+        self.edge_adding = None
+        # remove channel index
         indices = range(self.get_nchannels())
         del indices[channelidx]
         if len(indices) > 0:
             self.positions = np.vstack(self.get_positions()[indices,:])
             self.shanks = np.hstack(self.get_shanks()[indices])
+            # remove edges with that channel
+            edges = self.get_edges()
+            ind = (edges[:,0] != channelidx) & (edges[:,1] != channelidx)
+            if np.sum(ind) > 0:
+                self.edges = edges[ind,:]
         
     def move_channel(self, channelidx, position):
+        if channelidx is None:
+            return
         self.positions[channelidx, :] = position
         
     def get_image(self):
@@ -78,6 +106,17 @@ class Probe(Manager):
         counter = collections.Counter(shanks)
         nshanks = max(counter.keys()) + 1
         return nshanks
+        
+    def get_edges(self):
+        if not hasattr(self, 'edges'):
+            return np.zeros((0, 2), dtype=np.int32)
+        # print self.edges
+        # if self.edge_adding is not None:
+            # return np.vstack((self.edges, self.edge_adding))
+        return self.edges
+        
+    def get_edge_adding(self):
+        return self.edge_adding
         
     def get_text(self):
         return map(str, range(1, self.get_nchannels() + 1))
@@ -106,10 +145,13 @@ class ProbePM(PaintManager):
         # print colors.shape, colors.dtype
         # print list(colors)
         
+        # image
         self.add_visual(TextureVisual, texture=probe.get_image(), is_static=True,
                 mipmap=True,
                 minfilter='LINEAR_MIPMAP_NEAREST',
                 magfilter='LINEAR', name='probe_image')
+        
+        # channels
         self.add_visual(SpriteVisual, position=probe.get_positions(), 
             texture=get_disc(50), color=colors, name='channels')
         self.add_visual(TextVisual, coordinates=probe.get_positions(), 
@@ -117,13 +159,26 @@ class ProbePM(PaintManager):
             fontsize=18, letter_spacing=300.,
             color=get_color('w'), 
             name='channels_text')
+        
+        # edges
+        self.add_visual(PlotVisual,
+        
+            # position=RefVar('channels', 'position'), 
+            # index=probe.get_edges(),
+            position=probe.get_positions(), 
+            
+            # thickness=0.5,
+            color='r', primitive_type='LINES', name='edges')
+        
+        # shank text
         w, h = .2, .08
-        self.add_visual(RectanglesVisual,
-            coordinates=(-.8-w, .9-h, -.8+w, .9+h), color='k')
-        self.add_visual(TextVisual, coordinates=(-.8, .9), 
+        # self.add_visual(RectanglesVisual,
+            # coordinates=(-.8-w, .9-h, -.8+w, .9+h), color='k')
+        self.add_visual(TextVisual, coordinates=(-.55, .925), 
             text='Shank 0', 
             fontsize=24, #letter_spacing=300.,
-            color=get_color('y'), 
+            color=get_color('k'), 
+            letter_spacing=400.,
             name='shank_text')
     
     
@@ -132,13 +187,32 @@ class ProbeIM(InteractionManager):
         self.selected_channel = None
         self.shank = 0
         self.nshanks = 1
+        self.edge_current = None
         self.register(None, self.cancel)
         self.register('MoveChannel', self.move_channel)
         self.register('AddChannel', self.add_channel)
         self.register('RemoveChannel', self.remove_channel)
         self.register('AddShank', self.add_shank)
         self.register('NextShank', self.next_shank)
+        self.register('AddEdge', self.add_edge)
+        self.register('AddingEdge', self.adding_edge)
+        self.register('CancelAddingEdge', self.cancel_adding_edge)
+        self.register('Clear', self.clear)
 
+    def clear(self, param):
+        self.probe_manager.clear()
+        self.update_probe()
+        
+    def get_closest_channel(self, position, close_only=True):
+        probe = self.probe_manager
+        positions = probe.get_positions()
+        if positions.size > 0:
+            d = np.sum((positions - position) ** 2, axis=1)
+            channelidx = np.argmin(d)
+            if close_only and d[channelidx] > .01:
+                return
+            return channelidx
+        
     def move_channel(self, position):
         probe = self.probe_manager
         positions = probe.get_positions()
@@ -146,11 +220,13 @@ class ProbeIM(InteractionManager):
         if positions.size > 0:
             # select channel
             if self.selected_channel is None:
-                d = np.sum((positions - position) ** 2, axis=1)
-                channelidx = np.argmin(d)
-                if d[channelidx] > .01:
-                    return
-                self.selected_channel = channelidx
+                # d = np.sum((positions - position) ** 2, axis=1)
+                # channelidx = np.argmin(d)
+                # if d[channelidx] > .01:
+                    # return
+                channelidx = self.get_closest_channel(position)
+                if channelidx is not None:
+                    self.selected_channel = channelidx
             # or use already selected channel
             else:
                 channelidx = self.selected_channel
@@ -164,32 +240,91 @@ class ProbeIM(InteractionManager):
         probe = self.probe_manager
         probe.add_channel(position, self.shank)
         self.update_probe()
+        
+    def add_edge(self, position):
+        # print self.edge_current
+        if self.edge_current is None:
+            # keep the first node in memory for next click (creating an edge)
+            self.edge_current = self.get_closest_channel(position, close_only=False)
+            # print "1", self.edge_current
+        else:
+            probe = self.probe_manager
+            # create the edge
+            edge_second = self.get_closest_channel(position, close_only=False)
+            # print "2", edge_second
+            probe.add_edge(self.edge_current, edge_second)
+            self.edge_current = None
+            self.update_probe()
+    
+    def adding_edge(self, position):
+        if self.edge_current is not None:
+            probe = self.probe_manager
+            # create the edge
+            # edge_second = self.get_closest_channel(position, close_only=False)
+            # print "2", edge_second
+            probe.adding_edge(self.edge_current, position)
+            self.update_probe()
+        
+    def cancel_adding_edge(self, param):
+        if self.edge_current is not None:
+            self.edge_current = None
+            probe = self.probe_manager
+            probe.cancel_adding_edge()
+            self.update_probe()
     
     def remove_channel(self, position):
         probe = self.probe_manager
         positions = probe.get_positions()
         if positions.size > 0:
-            d = np.sum((positions - position) ** 2, axis=1)
-            channelidx = np.argmin(d)
-            if d[channelidx] > .01:
-                return
-            probe.remove_channel(channelidx)
-            self.update_probe()
+            # d = np.sum((positions - position) ** 2, axis=1)
+            # channelidx = np.argmin(d)
+            # if d[channelidx] > .01:
+                # return
+            channelidx = self.get_closest_channel(position)
+            if channelidx is not None:
+                probe.remove_channel(channelidx)
+                self.update_probe()
     
     def cancel(self, param=None):
+        # print "cancel"
+        probe = self.probe_manager
         self.selected_channel = None
+        # if self.edge_current is not None:
+            # self.edge_current = None
+        probe.edge_adding = None
         
     def update_probe(self):
         probe = self.probe_manager
         positions = probe.get_positions()
         colors = probe.get_colors()
         text = probe.get_text()
+        edges = probe.get_edges()
+        edges_positions = positions[edges.ravel(), :]
+        
+        edge_adding = probe.get_edge_adding()
+        if edge_adding is not None:
+            # print edges_positions, edge_adding
+            node0 = edge_adding[0]
+            pos_tmp = edge_adding[1]
+            # print node0, pos_tmp
+            edges_positions = np.vstack((
+                edges_positions,
+                positions[node0,:],
+                pos_tmp))
+        
         self.paint_manager.set_data(position=positions,
             color=colors,
             visual='channels')
         self.paint_manager.set_data(coordinates=positions, 
             text=text,
             visual='channels_text')
+        self.paint_manager.set_data(
+        
+            # index=edges,
+            position=edges_positions,
+            # nprimitives=edges.shape[0] / 2,
+            
+            visual='edges')
         
     def update_shank(self):
         probe = self.probe_manager
@@ -219,9 +354,16 @@ class ProbeBindings(Bindings):
             param_getter=lambda p: p['mouse_position'])
         self.set('RightClick', 'RemoveChannel',
             param_getter=lambda p: p['mouse_position'])
+        self.set('LeftClick', 'AddEdge', key_modifier='Control',
+            param_getter=lambda p: p['mouse_position'])
+        self.set('Move', 'AddingEdge', key_modifier='Control',
+            param_getter=lambda p: p['mouse_position'])
+        self.set('Move', 'CancelAddingEdge',)
+        self.set('RightClick', 'CancelAddingEdge', key_modifier='Control')
         self.set('KeyPress', 'AddShank', key='S')
         self.set('KeyPress', 'NextShank', key='Left', param_getter=-1)
         self.set('KeyPress', 'NextShank', key='Right', param_getter=1)
+        self.set('KeyPress', 'Clear', key='C')
     
     
 class ProbeWidget(GalryWidget):
