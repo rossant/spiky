Fixing bugs in computation of correlation matrix.

spiky/stats/correlations.py
@@ -105,21 +105,19 @@ def compute_correlations(features, clusters, masks,
     for ci in clusters_to_update:
         mui, Ci, Ciinv, logdeti, npointsi = stats[ci]
         for cj in clusterslist:
-            if ci > cj:
-                continue
             muj, Cj, Cjinv, logdetj, npointsj = stats[cj]
             dmu = (muj - mui).reshape((-1, 1))
             
             p = np.log(2*np.pi)*(-nDims/2.)+(-.5*np.log(np.linalg.det(Ci+Cj)))+(-.5)*np.dot(np.dot(dmu.T, np.linalg.inv(Ci+Cj)), dmu)
             alpha = float(npointsi) / nPoints
             matrix_product[ci, cj] = np.exp(p + np.log(alpha))
-            matrix_product[cj, ci] = matrix_product[ci, cj]
-    # s = matrix_product.sum(axis=1)
-    # matrix_product[s == 0, 0] = 1e-9
-    # s = matrix_product.sum(axis=1)
-    # matrix_product *= (1. / s.reshape((-1, 1)))
+            # matrix_product[cj, ci] = matrix_product[ci, cj]
     
-    return {(ci, cj): matrix_product[ci, cj]
-        for ci in clusterslist for cj in clusterslist}
+    d = {(ci, cj): matrix_product[ci, cj]
+        for ci in clusters_to_update for cj in clusterslist}
+    d.update({(ci, cj): matrix_product[ci, cj]
+        for ci in clusterslist for cj in clusters_to_update})
+    return d
+    
     
     

spiky/stats/indexed_matrix.py
@@ -47,10 +47,13 @@ class IndexedMatrix(object):
     # Indices
     # -------
     def add_indices(self, indices):
+        """Add new indices only if they don't already exist."""
         if isinstance(indices, (int, long)):
             indices = [indices]
         if len(indices) == 0:
             return
+        # Keep only those indices which do not exist already.
+        indices = self.not_in_indices(indices)
         # Raise an error if at least one requested index is already in the
         # current array indices.
         if np.any(np.in1d(indices, self.indices)):
@@ -129,7 +132,9 @@ class IndexedMatrix(object):
             indices_relative = indices_relative[0]
         return indices_relative
     
-    def not_in_indices(self, indices):
+    def not_in_indices(self, indices=None):
+        if indices is None:
+            indices = self.indices
         return sorted(set(indices) - set(self.indices))
     
     def blank_indices(self, indices=None):

spiky/stats/tests/test_indexed_matrix.py
@@ -54,13 +54,11 @@ def test_indexed_matrix_3():
     assert matrix.shape == (5, 5)
     assert np.array_equal(matrix.indices, [2, 3, 4, 5, 7])
     
-@raises(IndexError)
 def test_indexed_matrix_4():
     indices = [2, 3, 5, 7]
     matrix = IndexedMatrix(indices=indices)
-    
-    # The index already exists, so this raises an Exception.
     matrix.add_indices(7)
+    assert np.array_equal(matrix.indices, indices)
     
 def test_indexed_matrix_5():
     indices = [2, 3, 5, 7]